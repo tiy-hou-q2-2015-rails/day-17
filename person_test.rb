552ac8348a815fa51862b464ba99433dbde59163
Jesse Wolgamott <jesse@comalproductions.com>
2015-05-28 09:33:21 -0500

    Add email in init

--- person_test.rb
+++ person_test.rb
@@ -5,12 +5,12 @@ require './person'
 class PersonTest < MiniTest::Test
 
   def test_full_name
-    p = Person.new("jesse", "wolgamott", "1978-08-24")
+    p = Person.new("jesse", "wolgamott", "1978-08-24", "jwo@example.com")
     assert_equal "Wolgamott, Jesse", p.full_name
   end
 
   def test_age
-    p = Person.new("jesse", "wolgamott", "1978-08-24")
+    p = Person.new("jesse", "wolgamott", "1978-08-24", "jwo@example.com")
     assert_equal 36, p.age
   end
 end
