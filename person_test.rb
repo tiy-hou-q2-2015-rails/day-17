e1fb9ec3fc1c2f6ad69c0d9bb28236bb3f44d01b
Jesse Wolgamott <jesse@comalproductions.com>
2015-05-28 09:30:29 -0500

    Add test for person

---
+++ person_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './person'
+
+class PersonTest < MiniTest::Test
+
+  def test_full_name
+    p = Person.new("jesse", "wolgamott", "1978-08-24")
+    assert_equal "Wolgamott, Jesse", p.full_name
+  end
+
+  def test_age
+    p = Person.new("jesse", "wolgamott", "1978-08-24")
+    assert_equal 36, p.age
+  end
+end
